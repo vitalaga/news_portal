1b00ad209cf8673a6ea552076a7b910ccf5fb3c4
Vitaly <vitalage@yandex.ru>
2023-05-05 22:23:11 +0300

    added filters and search bar in the admin panel for posts

--- news_portal/admin.py
+++ news_portal/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Post, Author, Category, Comment
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'object_type', 'date_post', 'header', 'preview', 'post_rating']
+    list_filter = ['date_post', 'object_type', 'post_rating']
+    search_fields = ['name', 'category__name_category']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Comment)
